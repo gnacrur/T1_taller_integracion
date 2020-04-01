85346a3c5853e99ff73a457db67000f1dfd97f47
GONZALO NACRUR <gdnacrur@uc.cl>
2020-03-31 23:55:42 -0300

    subiendo todo

---
+++ t1_django_project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gnacr\OneDrive\documentos\2020-1\Taller de integración\T1_taller_integracion\t1_django_project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venvT1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gnacr\OneDrive\documentos\2020-1\Taller de integración\T1_taller_integracion\venvT1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
